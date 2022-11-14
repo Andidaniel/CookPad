610d8678e3db0dc6c238ed28628305f177b6c33e
Andrei Beres <andrei.beres@yahoo.com>
2022-11-14 21:37:37 +0200

    -Added Initial Project Files

---
+++ CookPad/CookPad/CookPad/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CookPad.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CookPad.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CookPad/CookPad/CookPad/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CookPad.Models;
+using CookPad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookPad.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
